2a01cdbfa6723c450c6decae8d6a6c930052df79
ExeLxrd <tech.mccune@gmail.com>
2023-09-29 21:09:41 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.412a41b12f68055a8f50e1b3ec6c187a.js"
+  "/precache-manifest.70a91de8bcb082ecb765e554b0c2e358.js"
 );
 
 self.addEventListener('message', (event) => {
